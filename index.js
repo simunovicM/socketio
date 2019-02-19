3a028ca3a002df9ea311029a602f7ec4275a3f2f
simunovicM <33186738+simunovicM@users.noreply.github.com>
2019-02-19 14:31:47 +0100

    Emit message

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var app = require('express')();
 var server = require('http').createServer(app);
+
+// app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // next();
+// });
+
 var io = require('socket.io').listen(server);
 
 var port = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
@@ -7,6 +14,10 @@ var port = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_P
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.emit('connection');
+  
+  socket.on('testMessage', function() {
+	 setTimeout(function() { socket.emit('hello', Math.random()) },1); 
+  });
 });
 
 server.listen(port, function(){
